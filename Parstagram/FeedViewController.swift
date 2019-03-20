f41c192ff88c28db5651b60056412762f85012d5
Rodrigo <rlepenneobelar17@students.desu.edu>
2019-03-19 20:59:23 -0400

    test

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -9,14 +9,16 @@
 import UIKit
 import Parse
 import AlamofireImage
+import MessageInputBar
 
 
 class FeedViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
-   
     
-
+    
+    
     @IBOutlet weak var tableView: UITableView!
-    
+    let commentBar = MessageInputBar()
+    var showsCommentBar = false
     var posts = [PFObject]()
     
     override func viewDidLoad() {
@@ -25,13 +27,27 @@ class FeedViewController: UIViewController, UITableViewDelegate,UITableViewDataS
         tableView.delegate = self
         tableView.dataSource = self
         
+        tableView.keyboardDismissMode = .interactive
+        
+        
         // Do any additional setup after loading the view.
+    }
+    func keyboardWillBeHidden(note: Notification){
+        let center = NotificationCenter.default
+    }
+    
+    override var inputAccessoryView: UIView? {
+        return commentBar
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return showsCommentBar
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         let query = PFQuery(className:"Posts")
-        query.includeKey("author")
+        query.includeKeys(["author","comments","comments.author"])
         query.limit = 20
         
         query.findObjectsInBackground { (posts, error) in
@@ -44,42 +60,97 @@ class FeedViewController: UIViewController, UITableViewDelegate,UITableViewDataS
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        let post = posts[section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        return comments.count + 2
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+        let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
         
+        if indexPath.row == 0{
+            
+            let user = post["author"] as! PFUser
+            
+            cell.usernameLabel.text = user.username
+            
+            cell.captionLabel.text = post["Captions"] as! String
+            
+            let imageFile = post["image"] as! PFFileObject
+            
+            let urlString = imageFile.url!
+            
+            let url = URL(string: urlString)!
+            
+            cell.photoView.af_setImage(withURL: url)
+            
+            
+            
+            return cell
+        }else if indexPath.row <= comments.count{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
+            let comment = comments[indexPath.row - 1]
+            
+            cell.commentLabel.text = comment["text"] as? String
+            
+            let user = comment["author"] as! PFUser
+            cell.nameLabel.text = user.username
+            
+            return cell
+        }else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "AddCommentCell")!
+            return cell
+        }
+        
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let post = posts[indexPath.row]
+        let comments = (post["comments"] as? [PFObject]) ?? []
         
-        let user = post["author"] as! PFUser
-        
-        cell.usernameLabel.text = user.username
-        
-        cell.captionLabel.text = post["Captions"] as! String
-        
-        let imageFile = post["image"] as! PFFileObject
-        
-        let urlString = imageFile.url!
-        
-        let url = URL(string: urlString)!
-        
-        cell.photoView.af_setImage(withURL: url)
-        
-        
-        
-        return cell
+        if indexPath.row == comments.count + 1 {
+            showsCommentBar = true
+            becomeFirstResponder()
+            commentBar.inputTextView.becomeFirstResponder()
+            
+            
+        }
+//        comment["text"] = "This is a random comment"
+//        comment["post"] = post
+//        comment["author"] = PFUser.current()!
+//
+//        post.add(comment, forKey: "comments")
+//        post.saveInBackground { (success, error) in
+//            if success{
+//                print ("comment saved")
+//            }else{
+//                print ("comment save error")
+//            }
+//        }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        
+        delegate.window?.rootViewController = loginViewController
+        
+        
     }
-    */
-
+    
+    
 }
+
+
+
+
